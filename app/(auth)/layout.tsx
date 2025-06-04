b7a9a7142a8000b6708ba79cf2bb3c1cec0b7fe2
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-06-04 12:15:13 +0530

    1st Commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {ReactNode} from "react";
+const AuthLayout = ({ children }: { children: ReactNode })=> {
+    return (
+        <div className={"auth-layout"}>{ children }</div>
+    )
+}
+export default AuthLayout
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+      <div className="bg-zinc-900">
+         HOME PAGE
+      </div>
+  );
+}
