217794a8c882e2fecbaf146c949d8bb12893891a
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-06-16 15:05:18 +0530

    1st Commit

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Agent from "@/components/Agent";
+const Page = () => {
+    return (
+        <>
+        <div>Interview Page</div>
+        <Agent userName={"You"} userId={"user1"} type={"generate"} />
+        </>
+    )
+}
+export default Page
