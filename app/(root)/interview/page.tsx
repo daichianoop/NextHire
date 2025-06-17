336830bdbc6d6d7d4136580a405bebefd2e0352a
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-06-17 14:13:10 +0530

    interview generation is live

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import Agent from "@/components/Agent";
-const Page = () => {
+import {getCurrentUser} from "@/lib/actions/auth.action";
+
+const Page = async () => {
+    const user = await getCurrentUser();
+
     return (
         <>
-        <div>Interview Page</div>
-        <Agent userName={"You"} userId={"user1"} type={"generate"} />
+            <h3>Interview Generation</h3>
+
+            <Agent userName={user?.name} userId={user?.id} type="generate" />
         </>
     )
 }
