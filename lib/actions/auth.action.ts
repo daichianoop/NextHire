9ff802df90726969cf8eedf0432a1727d9898cf7
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-06-17 15:12:17 +0530

    interview generation is live

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -48,6 +48,7 @@ export async function signIn(params: SignInParams) {
 
   try {
     const userRecord = await auth.getUserByEmail(email);
+    console.log(userRecord.uid);
 
     if(!userRecord) {
       return {
