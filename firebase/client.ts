6556f230d2e839b3f550b1657f0df18579dff1b5
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-06-23 05:57:09 +0530

    changed AuthForm

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBXU4q1UhUjxG7LGYkMkyHDVxNJAbGxcwI",
-    authDomain: "prepwise-e8286.firebaseapp.com",
-    projectId: "prepwise-e8286",
-    storageBucket: "prepwise-e8286.firebasestorage.app",
-    messagingSenderId: "457710422191",
-    appId: "1:457710422191:web:e9da5aa970f6b3d6f4ac5a",
-    measurementId: "G-T9WMCKMNH6"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
